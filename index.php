b6b225ab0404132add793c8a873f575dfea6e3b4
Riya-kumari-04 <riyakumari0339@gmail.com>
2024-05-25 19:22:33 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="/beginners-hub/css/styles.css">
+
+</head>
+
+<body>
+    <!-- *** Website Container Starts ***-->
+    <div class="Website-container">
+        <!-- **Home Section Starts ***-->
+        <section class="home" id="home">
+            <!-- ===main navbar starts===-->
+            <nav class="main-navbar">
+                <a href="#" class="logo">
+                    <img src="/beginners-hub/img/network.png" alt="">
+                    <h2>Beginners Hub</h2>
+                </a>
+                <ul class="nav-list">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="/beginners-hub/aboutus.php">About</a></li>
+                    <li><a href="/beginners-hub/signin.php">Course</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="/beginners-hub/contactus.php">Contact us</a></li>
+                </ul>
+                <a href="/beginners-hub/signin.php"id="/beginners-hub/livecode.php" class="let's-code-btn-container">
+                    <button class="lets-code-btn btn">Let's Code</button>
+                </a>
+                <div class="menu-btn">
+                    <span></span>
+                </div>
+            </nav>
+
+            <!-- ===main navbar Ends===-->
+            <!-- ==== Banner Starts ===-->
+            <div class="banner">
+
+                <div class="banner-desc">
+                    <h2>Start Learning With Our platform BeginnersHub, Anywhere, Anytime</h2>
+                    <p>Start your web development journey with this awsome website. This website various programmin language free of cost</p>
+                    <a href="/beginners-hub/signin.php" class="Get-Started-btn-container">
+                        <button class="Get-Started-btn btn">Get Started</button>
+                    </a>
+
+                </div>
+
+                <div class="banner-img">
+                    <div class="banner-img-container">
+                        <img src="/beginners-hub/img/hand-drawn-.jpg">
+
+                        <div class="states">
+                            <div class="total-courses">
+                                <div class="icon">
+                                    <i class="fa-solid fa-book"></i>
+                                </div>
+                                <div class="desc">
+                                    <span>284+</span>
+                                    <span>Total Courses</span>
+                                </div>
+                            </div>
+
+                            <div class="courses-ratings">
+                                <span>4.7<i class="fa-solid fa-star"></i></span>
+                                <span>Ratings (58k)</span>
+                            </div>
+                        </div>
+
+                        <div class="pattern">
+                            <img src="/beginners-hub/img/pattern.png">
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <!-- ==== Banner Ends ===-->
+
+
+
+        </section>
+        
+
+
+</body>
+
+</html>
